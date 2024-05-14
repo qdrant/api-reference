6d98084178768608e114c75a83b899768062b248
generall <andrey@vasnetsov.com>
2024-05-14 23:42:09 +0200

    use https repo

--- tools/sync-openapi.sh
+++ tools/sync-openapi.sh
@@ -12,7 +12,7 @@ PROJECT_ROOT="$(pwd)/$(dirname "$0")/../"
 
 rm -rf qdrant
 
-git clone --sparse --filter=blob:none --depth=1 -b master git@github.com:qdrant/qdrant.git
+git clone --sparse --filter=blob:none --depth=1 -b master https://github.com/qdrant/qdrant.git
 
 cd qdrant
 git sparse-checkout add docs/redoc
