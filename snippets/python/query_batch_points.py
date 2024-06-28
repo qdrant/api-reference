255786e03ffc91bdf9748baf1b7948cadc30ec57
Luis Cossío <luis.cossio@outlook.com>
2024-06-28 09:21:04 -0400

    add python snippets

---
+++ snippets/python/query_batch_points.py
@@ -0,0 +1,15 @@
+from qdrant_client import QdrantClient, models
+
+client = QdrantClient(url="http://localhost:6333")
+
+nearest = client.query_batch_points(
+    collection_name="{collection_name}",
+    requests=[
+        models.QueryRequest(
+            query=[0.01, 0.45, 0.67, ...],
+        ),
+        models.QueryRequest(
+            query=[0.11, 0.35, 0.6, ...],
+        ),
+    ]
+)
