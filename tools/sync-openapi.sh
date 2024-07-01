ae1e660f90c7de2f82752efecec38c34cec1a82c
generall <andrey@vasnetsov.com>
2024-07-01 23:40:18 +0200

    fix order of versions

--- tools/sync-openapi.sh
+++ tools/sync-openapi.sh
@@ -80,7 +80,7 @@ VERSIONS_TMP_YAML=qdrant/versions.tmp.yml
 
 echo "versions:" > $VERSIONS_TMP_YAML
 
-for version in $(ls $PROJECT_ROOT/fern/versions | grep -oP 'v\d.*' | sort -V); do
+for version in $(ls $PROJECT_ROOT/fern/versions | grep -oP 'v\d.*' | sort -h); do
     # cut the .yml extension
     version=$(echo $version | sed 's/\.yml$//g')
     echo "  - display-name: $version" >> $VERSIONS_TMP_YAML
